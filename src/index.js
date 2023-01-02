80d280037147e1b95ce7b1b9b5f8b4a5d345fd7b
Luca <lucaperullo@outlook.it>
2023-01-02 16:51:49 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import listEndpoints from 'express-list-endpoints'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const server = express() // inizzializzo il server con la libreria express
+const withelist = ["*"] // imposto la whitelist per consentire il cross origin resource sharing a tutti gli indirizzi
+const corsOptions = { // imposto le opzioni per il cors
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+const port = process.env.PORT || 3001 // imposto la porta del server
+server.use(cors(corsOptions)) // abilito il cors con le opzioni impostate sopra per consentire il cross origin resource sharing 
+server.use(cookieParser()) // abilito il cookie parser per leggere i cookie
+server.use(express.json()) // abilito il json parser per leggere i json inviati dal client al server 
+
+console.table(listEndpoints(server)) // stampo tutti gli endpoint disponibili nel server
+mongoose.set('strictQuery', true); // imposto la modalità strictQuery per evitare che vengano ignorati i parametri non previsti
+mongoose.connect(process.env.MONGO_CONNECTION).then(
+
+    server.listen(port, () => {
+        console.log(`Server is running away from ${port} zombies`)
+    })
+).catch(err => console.log(err)
+
+)
